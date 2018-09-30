224a37d4d51efbfc0cc806abe238a58e3306b5e5
Front End <chris@frontr.co.uk>
2018-09-30 14:22:00 +0100

    Media public_api update

--- frontr-sem-media.d.ts
+++ frontr-sem-media.d.ts
@@ -2,6 +2,5 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public_api';
-export { SlidePanelComponent as ɵc } from './lib/components/slide-panel';
 export { SemMaterialSharedModule as ɵa } from './lib/sem-material-shared.module';
 export { SemUiKitSharedModule as ɵb } from './lib/sem-ui-shared.module';
--- public_api.d.ts
+++ public_api.d.ts
@@ -5,7 +5,7 @@ export * from './lib/components/media-cropper/media-cropper.component';
 export * from './lib/components/media-edit/media-edit.component';
 export * from './lib/components/media-image-upload/media-upload.component';
 export * from './lib/components/slide-panel/slide-panel.component';
-export * from './lib/containers/sem-media-container/sem-media-container.component';
 export * from './lib/containers/panel/control-panel.component';
+export * from './lib/containers/sem-media-container/sem-media-container.component';
 export * from './lib/containers/settings/settings.component';
 export * from './lib/models/image';
