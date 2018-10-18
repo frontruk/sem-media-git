a2b479fc0602696eb8e6d92a850badda308f0afa
Front End <chris@frontr.co.uk>
2018-10-18 01:24:59 +0100

    addded video and media

--- lib/sem-video.service.d.ts
+++ lib/sem-video.service.d.ts
@@ -1,8 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 export declare class SemVideoService {
     protected readonly http: HttpClient;
     platformId: Object;
     constructor(http: HttpClient, platformId: Object);
-    fetchVideos(query: string): Observable<Object>;
+    fetchVideos(query: string): any;
 }
